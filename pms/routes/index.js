b3ccdc94068a587ab7833aeddd1bd8dbb6193616
Nikita <nikitamurmure@gmail.com>
2024-06-07 13:38:43 +0530

    change

--- pms/routes/index.js
+++ pms/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Password Management System' });
+});
+
+router.get('/signup', function (req, res, next) {
+  res.render('signup', { title: 'Password Management System' });
+});
+
+router.get('/password_category', function (req, res, next) {
+  res.render('password_category', { title: 'Password Management System' });
 });
 
 module.exports = router;
