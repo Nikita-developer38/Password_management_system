242da005b2bf6dbb9d66e73e741c148d51a24587
Nikita <nikitamurmure@gmail.com>
2024-06-08 10:18:53 +0530

    password management

--- pms/routes/index.js
+++ pms/routes/index.js
@@ -14,4 +14,15 @@ router.get('/password_category', function (req, res, next) {
   res.render('password_category', { title: 'Password Management System' });
 });
 
+router.get('/addNewCategory', function (req, res, next) {
+  res.render('addNewCategory', { title: 'Password Management System' });
+});
+
+router.get('/addNewPassword', function (req, res, next) {
+  res.render('addNewPassword', { title: 'Password Management System' });
+});
+
+router.get('/viewAllPassword', function (req, res, next) {
+  res.render('viewAllPassword', { title: 'Password Management System' });
+});
 module.exports = router;
